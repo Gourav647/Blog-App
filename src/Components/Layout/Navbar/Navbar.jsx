3d8ecd2faf9a38e5fe97877376222614514efe76
Gourav <gouravpaliwal196@gmail.com>
2024-12-22 07:26:47 +0530

    Working on Navbar

---
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Menu } from '@mui/icons-material'
+import { AppBar, IconButton, Stack, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <AppBar color='secondary'>
+      <Toolbar>
+        <Stack flexDirection={"row"} justifyContent={"space-between"} width={"100%"} alignItems={"center"}>
+        <Typography fontSize={25}>
+            Blog
+        </Typography>
+        <Stack>
+            
+        </Stack>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
