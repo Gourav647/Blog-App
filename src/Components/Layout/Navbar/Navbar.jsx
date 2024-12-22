e090d1004d657b62c0080b70aa94665dba9ef5c0
Gourav <gouravpaliwal196@gmail.com>
2024-12-22 14:48:47 +0530

    Add toggle functionality to Navbar for dashboard visibility

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import { Menu } from '@mui/icons-material'
 import { AppBar, IconButton, Stack, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../../../Context/Context'
 
 const Navbar = () => {
+  const {openDashboard,setOpenDashboard} = useContext(Context);
+  
   return (
     <AppBar color='secondary' position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
         <Stack flexDirection={"row"} justifyContent={"space-between"} width={"100%"} alignItems={"center"}>
+          <Stack>
+            <IconButton onClick={() => {setOpenDashboard(!openDashboard)}}>
+              <Menu/>
+            </IconButton>
         <Typography fontSize={25}>
             Blog
         </Typography>
+          </Stack>
         <Stack>
             
         </Stack>
