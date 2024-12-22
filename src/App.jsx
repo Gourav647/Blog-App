50067b5e6c83cc5c5bfc083567c42f8ad088c7c5
Gourav <gouravpaliwal196@gmail.com>
2024-12-22 07:49:32 +0530

    creating palletes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createTheme } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const Theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#2E0249"
+      },
+      secondary: {
+        main: "#570A57"
+      },
+      error: {
+        main: "#FF2929"
+      },
+      success: {
+        main: "#54B435"
+      },
+      info: {
+        main: "#C6E7FF"
+      },
+      text: {
+        main: "#171010"
+      }
+    }
+  })
   return (
     <>
       <div>
